ea90178a44193238dce1d464b028bddd23f22171
Kayan Chagas <kayan_edu@hotmail.com>
2021-03-23 00:38:23 -0300

    Created Dots

--- Assets/Scripts/Board.cs
+++ Assets/Scripts/Board.cs
@@ -8,14 +8,22 @@ public class Board : MonoBehaviour
     public int width;
     public int height;
 
+    public int borderSize;
+
     public GameObject tilePrefab;
+    public GameObject[] gamePiecePrefabs;
 
     Tile[,] m_allTiles;
+    GamePiece[,] m_allGamePieces;
 
     void Start()
     {
         m_allTiles = new Tile[width, height];
+        m_allGamePieces = new GamePiece[width, height];
+
         SetupTiles();
+        SetupCamera();
+        FillRandom();
     }
 
     void SetupTiles()
@@ -31,7 +39,63 @@ public class Board : MonoBehaviour
                 m_allTiles[i, j] = tile.GetComponent<Tile>();
 
                 tile.transform.parent = transform;
+                m_allTiles[i, j].Init(i, j, this);
             }
+        }
+    }
+
+    void SetupCamera()
+    {
+        Camera.main.transform.position = new Vector3((float)(width - 1)/2f, (float)(height - 1)/2f, -10f);
+
+        float aspectRatio = (float)Screen.width / (float)Screen.height;
+
+        float verticalSize = (float)height / 2f + (float)borderSize;
+
+        float horizontalSize = ((float)width / 2f + (float)borderSize) / aspectRatio;
+
+        Camera.main.orthographicSize = (verticalSize > horizontalSize) ? verticalSize : horizontalSize;
+    }
+
+    GameObject GetRandomGamePiece()
+    {
+        int randomIdx = Random.Range(0, gamePiecePrefabs.Length);
+
+        if (gamePiecePrefabs[randomIdx] == null)
+        {
+            Debug.LogWarning("Board: " + randomIdx + " does not contain a valid GamePiece prefab!");
+        }
+
+        return gamePiecePrefabs[randomIdx];
+    }
+
+    void PlaceGamePiece(GamePiece gamePiece, int x, int y)
+    {
+        if (gamePiece == null)
+        {
+            Debug.LogWarning("Board: invalid GamePiece!");
+            return;
+        }
+
+        gamePiece.transform.position = new Vector3(x, y, 0);
+        gamePiece.transform.rotation = Quaternion.identity;
+        gamePiece.SetCoord(x, y);
+    }
+
+    void FillRandom()
+    {
+        for(int i = 0; i < width; i++)
+        {
+            for (int j = 0; j < height; j++)
+            {
+                GameObject randomPiece = Instantiate(GetRandomGamePiece(), Vector3.zero, Quaternion.identity) as GameObject;
+
+                if(randomPiece != null)
+                {
+                    PlaceGamePiece(randomPiece.GetComponent<GamePiece>(), i, j);
+                }
+            }
+
         }
     }
 }
--- Assets/Scripts/GamePiece.cs
+++ Assets/Scripts/GamePiece.cs
@@ -2,23 +2,27 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Tile : MonoBehaviour
+public class GamePiece : MonoBehaviour
 {
 
     public int xIndex;
     public int yIndex;
 
-    Board m_board;
-
+    // Start is called before the first frame update
     void Start()
     {
         
     }
 
-    public void Init(int x, int y, Board board)
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    public void SetCoord(int x, int y)
     {
         xIndex = x;
         yIndex = y;
-        m_board = board;
     }
 }
